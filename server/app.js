9e9e3d1a26ea29ab6d5317d6a9fb08283724705e
Sunder Rawat <sunderrawat777@gmail.com>
2021-08-26 22:59:40 +0530

    backend signup and login api create

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
+//parse req body data
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.status(200).send('App is started...')
+//home url dummy response
+app.get("/", (req, res) => {
+  res.status(200).send("App is started...");
+});
+
+//RESTfull api routes
+app.use("/api/v1/users", userRouter);
+
+//404 not found error
+app.use('/api/v1/*', (req, res)=>{
+    res.status(404).json({status: 'fail', statusCode: 404, message: 'this api route is not find or not defined by developer'})
 })
 
 module.exports = app;
---
+++ server/controllers/authController.js
@@ -0,0 +1,59 @@
+const User = require("./../model/userModel");
+
+exports.signUp = async (req, res) => {
+  try {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
+    res.status(201).json({
+      status: "success",
+      statusCode: 201,
+      message: "User signUp successfully",
+      data: newUser,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      statusCode: 500,
+      message: `something went wrong ${err.message}`,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await User.find({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    if (!user) {
+      return res
+        .status(404)
+        .json({
+          status: "fail",
+          statusCode: 404,
+          message: "Incorrect email or password",
+        });
+    }
+    res
+      .status(200)
+      .json({
+        status: "success",
+        statusCode: 200,
+        message: "user successfully login",
+        data: user,
+      });
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        status: "fail",
+        statusCode: 500,
+        message: "something went wrong",
+      });
+  }
+};
